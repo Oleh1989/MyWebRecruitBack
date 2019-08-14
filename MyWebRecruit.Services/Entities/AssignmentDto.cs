2e6e77752a217d458322e735d80abcf6e3a9f0c6
Олег Бабков <babkoff1989@gmail.com>
2019-08-14 16:22:51 +0300

    Check

---
+++ MyWebRecruit.Services/Entities/AssignmentDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyWebRecruit.Services.Entities
+{
+    public class AssignmentDto
+    {
+        public int Id { get; set; }
+
+        public DateTime StartDt { get; set; }
+        public DateTime? EndDt { get; set; }
+        public decimal Salary { get; set; }
+        public int AssigType { get; set; }
+        public string JobName { get; set; }
+        public string ReasonLeave { get; set; }
+        public int CandId { get; set; }
+        public int JobId { get; set; }
+
+        // IDeletable interface
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ MyWebRecruit.Services/Interfaces/IAssignmentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyWebRecruit.Services.Entities;
+
+namespace MyWebRecruit.Services.Interfaces
+{
+    public interface IAssignmentService
+    {
+        IQueryable<AssignmentDto> GetAssignmentList(JobDto job, CandidateDto candidate);
+    }
+}
